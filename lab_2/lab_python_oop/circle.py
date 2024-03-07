bdf585af87a42e90b2555dd670b664d2bef912c0
vika.udalova.2000@mail.ru <vika.udalova.2000@mail.ru>
2024-03-07 09:16:43 +0300

    lab_1

---
+++ lab_2/lab_python_oop/circle.py
@@ -0,0 +1,18 @@
+from lab_2.lab_python_oop.figure import Figure
+from lab_2.lab_python_oop.color import Color
+import math
+class Circle(Figure):
+    type = "Круг"
+    def __init__(self, radius, color):
+        self.radius = radius
+        self.c_color = Color(color)
+    def area(self):
+       return math.pi * (self.radius ** 2)
+
+    def __repr__(self):
+        return '{} {} цвета радиусом {} площадью {}.'.format(
+            self.type,
+            self.c_color._color,
+            self.radius,
+            self.area()
+        )
---
+++ lab_2/lab_python_oop/rectangle.py
@@ -0,0 +1,19 @@
+from lab_2.lab_python_oop.figure import Figure
+from lab_2.lab_python_oop.color import Color
+class Rectangle(Figure):
+    type = "Прямоугольник"
+    def __init__(self, width, length, color):
+        self.width = width
+        self.length = length
+        self.r_color = Color(color)
+    def area(self):
+        return self.width * self.length
+    def __repr__(self):
+       return '{} {} цвета с длиной стороны {}, шириной стороны {}, площадью {}.'.format(
+            self.type,
+            self.r_color._color,
+            self.length,
+            self.width,
+            self.area()
+        )
+
